5fa3404c30902c3b30c29058ae5e275fd0dd6c21
tlucanti <prototype3628800@yandex.ru>
2023-05-01 10:42:17 +0000

    introduce role and temperature selection

--- GPT.py
+++ GPT.py
@@ -1,39 +1,60 @@
 
 import openai
 from Color import Color
+from Color import log
 
 class GPT():
 
-    class GPTRole():
-        class User(): pass
-
     def __init__(self, role=None):
+        if role is None:
+            #role = 'You are an article writing assistant'
+            role = 'You are a intelligent assistant'
         openai.api_key = self._get_token()
         self.model = 'gpt-3.5-turbo'
-        if role is None:
-            role = 'You are a intelligent assistant'
+        self.temperature = 1
+        self.temp_value = 'normal'
+        self.role = role
         self.messages = [{
             'role': 'system',
-            'content': role}]
+            'content': self.role}]
 
-    def query(self, prompt, role='user', temperature=0.5):
+    def query(self, prompt, role='user'):
         self.messages.append({
             'role': 'user',
             'content': prompt
         })
         response = openai.ChatCompletion.create(
-            model=self.model, messages=self.messages
+            model=self.model, messages=self.messages, temperature=self.temperature
         )
         Color.timestamp()
         try:
             reply = response.choices[0].message.content
+            log(str(response))
         except Exception:
-            print(Color.R(str(response)))
+            log(Color.R(str(response)))
             return 'ERROR\n' + str(response)
         self.messages.append({
-            "role": "assistant",
-            "content": reply})
+            'role': 'assistant',
+            'content': reply})
         return reply
+
+    def reset(self):
+        self.messages = [{
+            'role': 'system',
+            'content': self.role}]
+
+    def get_temperature(self):
+        return self.temp_value
+
+    def set_temperature(self, num, value):
+        self.temperature = num
+        self.temp_value = value
+
+    def get_role(self):
+        return self.role
+
+    def set_role(self, role):
+        self.role = role
 
     def _get_token(self):
         try:
@@ -47,8 +68,10 @@ class GPT():
             sys.exit(1)
 
 if __name__ == '__main__':
-    gpt = GPT()
+    gpt = GPT('assistant for solving economic problems')
     while True:
-        prompt = input('>>> ')
-        reply = gpt.query(prompt)
-        print('<<<', reply)
+        input()
+        with open('test.txt') as f:
+            prompt = f.read()
+        reply = gpt.query(prompt, 0)
+        log('<<<', reply)
--- SimpleGPTbot.py
+++ SimpleGPTbot.py
@@ -1,22 +1,38 @@
 
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder
+from telegram.ext import ContextTypes, CommandHandler, ConversationHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, MenuButtonCommands
 from GPT import GPT
 from collections import defaultdict
 from Color import Color
 from Color import log
 
 class SimpleGPTbot():
-    def __init__(self):
-        self.token = self._get_token()
+    WAIT_FOR_ROLE = 0x1
+
+    def __init__(self, token=None):
+        if token is None:
+            self.token = self.get_token()
+        else:
+            self.token = token
         self.application = ApplicationBuilder().token(self.token).build()
         start_handler = CommandHandler('start', self.new_client)
-        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), self.handler)
+        echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self.handler)
         reset_handler = CommandHandler('reset', self.reset)
-        self.application.add_handler(echo_handler)
-        self.application.add_handler(reset_handler)
-        self.application.add_handler(start_handler)
-
+        help_handler = CommandHandler('help', self.help)
+        temperature_handler = CommandHandler('temp', self.temp)
+        button_handler = CallbackQueryHandler(self.button)
+        role_handler = ConversationHandler(
+            entry_points=[CommandHandler('role', self.role)],
+            states={
+                self.WAIT_FOR_ROLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.select_role)]
+            },
+            fallbacks=[CommandHandler('cancel', self.cancel)]
+        )
+        for handler in start_handler, role_handler, reset_handler, help_handler, \
+            temperature_handler, echo_handler, button_handler:
+                self.application.add_handler(handler)
         self.users = defaultdict(GPT)
 
     def start(self):
@@ -35,21 +51,112 @@ class SimpleGPTbot():
 
     async def reset(self, update, context):
         chat_id = update.effective_chat.id
-        self.users[chat_id] = GPT()
+        self.users[chat_id].reset()
         Color.timestamp()
         log(Color.P('RESET') + Color.W(f' : {chat_id}'))
         await context.bot.send_message(chat_id=chat_id, text='reset completed')
 
+    async def temp(self, update, context):
+        chat_id = update.effective_chat.id
+        user = self.users[chat_id]
+        Color.timestamp()
+        log('temp')
+        keyboard = [[
+            InlineKeyboardButton('low', callback_data='temp_low'),
+            InlineKeyboardButton('normal', callback_data='temp_normal'),
+            InlineKeyboardButton('high', callback_data='temp_high')
+        ]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        text = f'select temperature value (current is {user.get_temperature()})'
+        msg = await update.message.reply_text(text, reply_markup=reply_markup)
+
+    async def role(self, update, context):
+        chat_id = update.effective_chat.id
+        user = self.users[chat_id]
+        Color.timestamp()
+        log('role')
+        text = 'select new role (or type /cancel). current role is:\n' + user.get_role()
+        await context.bot.send_message(chat_id=chat_id, text=text)
+        return self.WAIT_FOR_ROLE
+
+    async def cancel(self, update, context):
+        chat_id = update.effective_chat.id
+        Color.timestamp()
+        log('cancel role change')
+        await context.bot.send_message(chat_id=chat_id, text='cancelled')
+        return ConversationHandler.END
+
+    async def select_role(self, update, context):
+        chat_id = update.effective_chat.id
+        user = self.users[chat_id]
+        Color.timestamp()
+        role_text = update.message.text
+        log('selected role: ' + role_text)
+        user.set_role(role_text)
+        user.reset()
+        await context.bot.send_message(chat_id=chat_id, text='selected new role')
+        return ConversationHandler.END
+
+    async def button(self, update, context):
+        chat_id = update.effective_chat.id
+        user = self.users[chat_id]
+        query = update.callback_query
+        await query.answer()
+        data = query.data
+        Color.timestamp()
+        log(f'temp selected {data}')
+        if data == 'temp_low':
+            user.set_temperature(0.2, 'low')
+            data = 'low'
+        elif data == 'temp_normal':
+            user.set_temperature(1, 'normal')
+            data = 'normal'
+        elif data == 'temp_high':
+            user.set_temperature(1.5, 'high')
+            data = 'high'
+        else:
+            Color.FAILED(f'unknown button {data}')
+        text=f'selected temperature: {data}'
+        await query.edit_message_text(text=text)
+
+
+    async def help(self, update, context):
+        chat_id = update.effective_chat.id
+        self.users[chat_id] = GPT()
+        Color.timestamp()
+        log('help')
+        text = 'help:\n' \
+            '\n' \
+            '/start\n' \
+            '  start conversation\n' \
+            '/help\n' \
+            '  prints this message\n' \
+            '/reset\n' \
+            '  reset gpt instance\n' \
+            '/temp\n' \
+            '  change temperature value\n' \
+            '/role\n' \
+            '  change gpt role'
+        await context.bot.send_message(chat_id=chat_id, text=text)
+
     async def new_client(self, update, context):
         chat_id = update.effective_chat.id
         request = update.message.text[7:].strip()
-
         Color.timestamp()
         log(Color.P('NEW CLIENT') + Color.W(f' : {chat_id}'))
+        await context.bot.set_my_commands([
+            ('/start', 'start'),
+            ('/help', 'manual'),
+            ('/reset', 'reset session'),
+            ('/temp', 'select temperature'),
+            ('/role', 'change chat role')
+        ])
+        await context.bot.set_chat_menu_button(chat_id=chat_id, menu_button=MenuButtonCommands())
 
-    def _get_token(self):
+    @staticmethod
+    def get_token(path='./.simplebot.token'):
         try:
-            f = open('./.simplebot.token', 'r')
+            f = open(path, 'r')
             token = f.read().strip()
             f.close()
             return token
@@ -58,5 +165,7 @@ class SimpleGPTbot():
             sys.exit(1)
 
 if __name__ == '__main__':
+    #token = SimpleGPTbot.get_token('.telegram.token')
+    #bot = SimpleGPTbot(token)
     bot = SimpleGPTbot()
     bot.start()
